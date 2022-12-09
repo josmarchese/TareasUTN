1084911a889a7343494c7f9646aef62b3e4be9eb
Jo Marchese <josefina.marchese@gmail.com>
2022-12-09 00:03:28 -0300

    tarea m4u3

--- M4U3/app.js
+++ M4U3/app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+var pool = require('./models/bd');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,6 +24,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// select 
+pool.query('select * from postulantes').then(function (resultados) {
+  console.log(resultados)
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
